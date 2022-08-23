21d4d006e7cbbfcdb1c5e2bc82c2265363a32ef1
MANU <manuhunt007@gmail.com>
2022-08-23 15:39:51 +0530

    SportyShoeProject

---
+++ SportyShoes.com/src/main/java/com/example/demo/ProductDao.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductDao {
+
+	@Autowired
+    ProductRepo repo;
+	
+	
+	public Product insert(Product p) {
+		
+		return repo.save(p);
+	}
+	
+	
+	public List<Product> getall(){
+		return repo.findAll();
+	}
+	
+	
+	public Product update(Product s) {
+		
+		Product pp=repo.findById(s.getId()).orElse(null);
+		            
+		pp.setProduct(s.getProduct());
+		return repo.save(pp);
+	}
+
+	public void deleteByid(int id) {
+		repo.deleteById(id);
+		
+	}
+
+}
